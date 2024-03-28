90f309da06e9f3dd587841ca99882ff783617e18
Nauman <nnauman1991@gmail.com>
2024-03-29 02:20:53 +0500

    Update api.tsx
    Update api.tsx

--- api/api.tsx
+++ api/api.tsx
@@ -11,9 +11,7 @@ export async function parseDocument() {
     success: false,
   };
   try {
-    // const file = await fs.readFile(process.cwd() + '/api/data.csv', 'utf8');
-    // const file = await fs.readFile('api/data.csv', 'utf8');
-    const file = await fs.readFile('public/data.csv', 'utf8');
+    const file = await fs.readFile(process.cwd() + '/api/data.csv', 'utf8');
     let stringData = file.split(',');
     let data = stringData[0].split('\n');
 
@@ -23,10 +21,9 @@ export async function parseDocument() {
     return response;
   } catch (error) {
     console.log(error);
-    response.data =
-      '["2023-06-25 11:00;1abc.txt","2023-06-25 12:00;abc.txt","2023-06-25 13:00;01abc.txt","2023-06-25 14:00;0010abc.txt","2023-06-25 15:00;011abc.txt","2023-06-25 16:00;20-abc.txt","2023-06-25 17:00;021-abc.txt","2023-06-25 18:00;002-abc.txt","2023-06-25 19:00;cba.txt","2023-06-25 20:00;abc010.txt","2023-06-25 21:00;abc1.txt"]';
-    response.status = '200';
-    response.success = true;
+    response.data = 'Internal Server Error';
+    response.status = '500';
+    response.success = false;
     return response;
   }
 }
