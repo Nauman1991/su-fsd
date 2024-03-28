f85ce597b2e21e2b64d50dbef971781b0a783049
Nauman <nnauman1991@gmail.com>
2024-03-29 02:12:55 +0500

    json update
    json update

--- api/api.tsx
+++ api/api.tsx
@@ -23,17 +23,8 @@ export async function parseDocument() {
     return response;
   } catch (error) {
     console.log(error);
-    response.data = JSON.stringify(`2023-06-25 11:00;1abc.txt
-    2023-06-25 12:00;abc.txt
-    2023-06-25 13:00;01abc.txt
-    2023-06-25 14:00;0010abc.txt
-    2023-06-25 15:00;011abc.txt
-    2023-06-25 16:00;20-abc.txt
-    2023-06-25 17:00;021-abc.txt
-    2023-06-25 18:00;002-abc.txt
-    2023-06-25 19:00;cba.txt
-    2023-06-25 20:00;abc010.txt
-    2023-06-25 21:00;abc1.txt`);
+    response.data =
+      '["2023-06-25 11:00;1abc.txt","2023-06-25 12:00;abc.txt","2023-06-25 13:00;01abc.txt","2023-06-25 14:00;0010abc.txt","2023-06-25 15:00;011abc.txt","2023-06-25 16:00;20-abc.txt","2023-06-25 17:00;021-abc.txt","2023-06-25 18:00;002-abc.txt","2023-06-25 19:00;cba.txt","2023-06-25 20:00;abc010.txt","2023-06-25 21:00;abc1.txt"]';
     response.status = '200';
     response.success = true;
     return response;
